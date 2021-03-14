4fa241b596fa0063d0e7b4739fe34cd31e263ab1
MGamer404 <80640340+MGamer404@users.noreply.github.com>
2021-03-14 23:33:22 +0300

    Add files via upload

---
+++ discord1/commands/recitation4.js
@@ -0,0 +1,36 @@
+const ytdl = require('ytdl-core');
+const ytSearch = require('yt-search');
+ 
+module.exports = {
+    name: '4اقرا',
+    description: 'Joins and plays a video from youtube',
+    async execute(message, args) {
+        const voiceChannel = message.member.voice.channel;
+ 
+ 
+        const validURL = (str) =>{
+            var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+            if(!regex.test(str)){
+                return false;
+            } else {
+                return true;
+            }
+        }
+ 
+        if(validURL(args[0])){
+ 
+            const  connection = await voiceChannel.join();
+            const stream  = ytdl(args[0], {filter: 'audioonly'});
+ 
+            connection.play(stream, {seek: 0, volume: 1})
+            .on('finish', () =>{
+                message.channel.send('$4اقرا https://www.youtube.com/watch?v=T3a6ZTbctWw');
+            });
+ 
+            await message.reply(`جاري تشغيل السورة`)
+ 
+            return
+        }
+ 
+    }
+}
---
+++ discord1/commands/recitation5.js
@@ -0,0 +1,36 @@
+const ytdl = require('ytdl-core');
+const ytSearch = require('yt-search');
+ 
+module.exports = {
+    name: '5اقرا',
+    description: 'Joins and plays a video from youtube',
+    async execute(message, args) {
+        const voiceChannel = message.member.voice.channel;
+ 
+ 
+        const validURL = (str) =>{
+            var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+            if(!regex.test(str)){
+                return false;
+            } else {
+                return true;
+            }
+        }
+ 
+        if(validURL(args[0])){
+ 
+            const  connection = await voiceChannel.join();
+            const stream  = ytdl(args[0], {filter: 'audioonly'});
+ 
+            connection.play(stream, {seek: 0, volume: 1})
+            .on('finish', () =>{
+                message.channel.send('$5اقرا https://www.youtube.com/watch?v=MGEWrAtHFwU');
+            });
+ 
+            await message.reply(`جاري تشغيل السورة`)
+ 
+            return
+        }
+ 
+    }
+}
