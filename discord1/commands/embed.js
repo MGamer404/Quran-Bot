8eaab58cc005c5612015634cfb6d26330bfb8a5e
MGamer404 <80640340+MGamer404@users.noreply.github.com>
2021-03-14 21:02:01 +0300

    Update embed.js

--- discord1/commands/embed.js
+++ discord1/commands/embed.js
@@ -6,7 +6,8 @@ module.exports = {
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#304281')
         .setTitle('اسماء القراء')
-        .setDescription('الرجاء كتابة الرمز "$" وبجانبه الرقم الموضح جانب اسم القارئ بالصوره');
+        .setDescription('الرجاء كتابة الرمز "$" وبجانبه الرقم الموضح جانب اسم القارئ بالصوره')
+        .setImage('image.png');
         
         message.channel.send(newEmbed);
     }
